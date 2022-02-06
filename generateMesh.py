babb972e88f88f33c0139979887fbab6895e0b8a
ueuzj <ueuzj@student.kit.edu>
2022-02-06 20:12:17 +0100

    Final way to create mesh for Castem

--- generateMesh.py
+++ generateMesh.py
@@ -7,11 +7,11 @@ from meshes import primitives
 from utils.viz import plot_mesh#, plot_vector, plot_scalar
 
 # Mesh
-Lx = 5
-Ly = 10
-s=0.5
-L0=1
-seedDist=0.5
+Lx = 100
+Ly = 400
+s=2
+L0=30
+seedDist=1
 
 geom_type = "bar"
 
@@ -23,13 +23,23 @@ gmsh_model, tdim = primitives.mesh_ep_gmshapi(geom_type,
                                     seedDist, 
                                     sep=0.1,
                                     tdim=2)
-
+"""
+# Only needed when we are generating a mesh to validate the calculations
+gmsh_model, tdim = primitives.mesh_rightCrack_gmshapi(geom_type,
+                                    Lx, 
+                                    Ly,
+                                    L0, 
+                                    s,   
+                                    seedDist, 
+                                    sep=0.1,
+                                    tdim=2)
+"""
 mesh, mts = meshes.gmsh_model_to_mesh(gmsh_model,
                                cell_data=False,
                                facet_data=True,
                                gdim=2, 
                                exportMesh=True, 
-                               fileName="epTestMesh.msh")
+                               fileName="twoCrack_ep2.unv")
 
 plt.figure()
 ax = plot_mesh(mesh)
