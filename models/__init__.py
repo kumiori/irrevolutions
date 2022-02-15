5a6719a08f5c4519db88932ebe9e0b4ab1f7144b
ueuzj <ueuzj@student.kit.edu>
2022-02-15 20:26:19 +0100

    Created first example phase field-EP

--- models/__init__.py
+++ models/__init__.py
@@ -17,15 +17,15 @@ class ElasticityModel:
     # Basic class for elasticity
 
     def __init__(self, eps_0=None, model_parameters={}):
-        self.model_parameters = default_parameters["model"]
-        if model_parameters:
-            self.model_parameters.update(model_parameters)
         """
         Initializes the sound material parameters.
         * Sound material parameters:
             - model_parameters["E"]: sound Young modulus
             - model_parameters["nu"]: sound Poisson ratio
         """
+        self.model_parameters = default_parameters["model"]
+        if model_parameters:
+            self.model_parameters.update(model_parameters)
         # Sound material paramaters
         self.E = self.model_parameters["E"]
         self.nu = self.model_parameters["nu"]
@@ -45,7 +45,7 @@ class ElasticityModel:
         if self.model_type == "plane-strain":
             return ufl.sym(
                 ufl.as_matrix([
-                    [u[0].dx(0), u[0].dx(1), 0],
+                    [u[0].dx(0), u[0.].dx(1), 0],
                     [u[1].dx(0), u[1].dx(1), 0],
                     [0, 0, 0],
                 ]))
@@ -77,103 +77,104 @@ class ElasticityModel:
         return self.elastic_energy_density(state)
 
 
-# class DamageElasticityModel(ElasticityModel):
-#     """
-#     Base class for elasticity coupled with damage.
-#     """
-#     def __init__(self, model_parameters={}):
-#         """
-#         Initializes the sound material parameters.
-#         * Sound material parameters:
-#             - E_0: sound Young modulus
-#             - nu_0: sound plasticity ratio
-#             - sig_d_0: sound damage yield stress
-#             - ell: internal length
-#             - k_res: fully damaged stiffness modulation
-#         """
-#         # Initialize the elastic parameters
-#         super().__init__(model_parameters)
+class DamageElasticityModel(ElasticityModel):
+    """
+    Base class for elasticity coupled with damage.
+    """
 
-#         # Initialize the damage parameters
-#         self.w1 = self.model_parameters["w1"]
-#         self.ell = self.model_parameters["ell"]
-#         self.k_res = self.model_parameters["k_res"]
+    def __init__(self, model_parameters={}):
+        """
+        Initializes the sound material parameters.
+        * Sound material parameters:
+            - E_0: sound Young modulus
+            - nu_0: sound plasticity ratio
+            - sig_d_0: sound damage yield stress
+            - ell: internal length
+            - k_res: fully damaged stiffness modulation
+        """
+        # Initialize the elastic parameters
+        super().__init__(model_parameters)
 
-#     def a(self, alpha):
-#         k_res = self.k_res
-#         return (1 - alpha)**2 + k_res
+        # Initialize the damage parameters
+        self.w1 = self.model_parameters["w1"]
+        self.ell = self.model_parameters["ell"]
+        self.k_res = self.model_parameters["k_res"]
 
-#     def w(self, alpha):
-#         """
-#         Return the dissipated energy function as a function of the state
-#         (only depends on damage).
-#         """
-#         # Return w(alpha) function
-#         return alpha
+    def a(self, alpha):
+        k_res = self.k_res
+        return (1 - alpha)**2 + k_res
 
-#     def elastic_energy_density_strain(self, eps, alpha):
-#         """
-#         Returns the elastic energy density from the strain and the damage.
-#         """
-#         # Parameters
-#         lmbda = self.lmbda
-#         mu = self.mu
-#         energy_density = (
-#             self.a(alpha) * 1.0 / 2.0 *
-#             (2 * mu * ufl.inner(eps, eps) + lmbda * ufl.tr(eps)**2))
-#         return energy_density
+    def w(self, alpha):
+        """
+        Return the dissipated energy function as a function of the state
+        (only depends on damage).
+        """
+        # Return w(alpha) function
+        return alpha
 
-#     def elastic_energy_density(self, state):
-#         """
-#         Returns the elastic energy density from the state.
-#         """
-#         # Parameters
-#         alpha = state["alpha"]
-#         u = state["u"]
-#         eps = self.eps(u)
-#         return self.elastic_energy_density_strain(eps, alpha)
+    def elastic_energy_density_strain(self, eps, alpha):
+        """
+        Returns the elastic energy density from the strain and the damage.
+        """
+        # Parameters
+        lmbda = self.lmbda
+        mu = self.mu
+        energy_density = (
+            self.a(alpha) * 1.0 / 2.0 *
+            (2 * mu * ufl.inner(eps, eps) + lmbda * ufl.tr(eps)**2))
+        return energy_density
 
-#     def stress(self, strain, alpha):
-#         # Differentiate the elastic energy w.r.t. the strain tensor
-#         eps_ = ufl.variable(strain)
-#         # Derivative of energy w.r.t. the strain tensor to obtain the stress
-#         # tensor
-#         sigma = ufl.diff(self.elastic_energy_density_strain(eps_, alpha), eps_)
-#         return sigma
+    def elastic_energy_density(self, state):
+        """
+        Returns the elastic energy density from the state.
+        """
+        # Parameters
+        alpha = state["alpha"]
+        u = state["u"]
+        eps = self.eps(u)
+        return self.elastic_energy_density_strain(eps, alpha)
 
-#     def stress0(self, u):
-#         strain = self.eps(u)
-#         lmbda = self.lmbda
-#         mu = self.mu
-#         sigma = 2 * mu * strain + lmbda * ufl.tr(strain) * ufl.Identity(
-#             self.model_dimension)
-#         return sigma
+    def stress(self, strain, alpha):
+        # Differentiate the elastic energy w.r.t. the strain tensor
+        eps_ = ufl.variable(strain)
+        # Derivative of energy w.r.t. the strain tensor to obtain the stress
+        # tensor
+        sigma = ufl.diff(self.elastic_energy_density_strain(eps_, alpha), eps_)
+        return sigma
 
-#     def damage_dissipation_density(self, state):
-#         """
-#         Return the damage dissipation density from the state.
-#         """
-#         # Get the material parameters
-#         self.E
-#         w1 = self.w1
-#         ell = self.ell
-#         # Get the damage
-#         alpha = state["alpha"]
-#         # Compute the damage gradient
-#         grad_alpha = ufl.grad(alpha)
-#         # Compute the damage dissipation density
-#         D_d = w1 * self.w(alpha) + w1 * ell**2 * ufl.dot(
-#             grad_alpha, grad_alpha)
-#         return D_d
+    def stress0(self, u):
+        strain = self.eps(u)
+        lmbda = self.lmbda
+        mu = self.mu
+        sigma = 2 * mu * strain + lmbda * ufl.tr(strain) * ufl.Identity(
+            self.model_dimension)
+        return sigma
 
-#     def total_energy_density(self, state):
-#         """
-#         Return the damage dissipation density from the state.
-#         """
-#         # Get the material parameters
-#         energy = self.elastic_energy_density(
-#             state) + self.damage_dissipation_density(state)
-#         return energy
+    def damage_dissipation_density(self, state):
+        """
+        Return the damage dissipation density from the state.
+        """
+        # Get the material parameters
+        self.E
+        w1 = self.w1
+        ell = self.ell
+        # Get the damage
+        alpha = state["alpha"]
+        # Compute the damage gradient
+        grad_alpha = ufl.grad(alpha)
+        # Compute the damage dissipation density
+        D_d = w1 * self.w(alpha) + w1 * ell**2 * ufl.dot(
+            grad_alpha, grad_alpha)
+        return D_d
+
+    def total_energy_density(self, state):
+        """
+        Return the damage dissipation density from the state.
+        """
+        # Get the material parameters
+        energy = self.elastic_energy_density(
+            state) + self.damage_dissipation_density(state)
+        return energy
 
 
 # class ThinFilmModel(DamageElasticityModel):
---
+++ solveModel/solvePF.py
@@ -0,0 +1,310 @@
+# library include
+
+
+import numpy as np
+import yaml
+import json
+import sys
+import os
+from pathlib import Path
+
+from mpi4py import MPI
+
+import petsc4py
+from petsc4py import PETSc
+
+import dolfinx
+import dolfinx.plot
+from dolfinx import log
+import ufl
+import models
+from models import DamageElasticityModel as Brittle
+import algorithms
+from algorithms import am
+
+from dolfinx.io import XDMFFile
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+import dolfinx
+import dolfinx.plot
+import dolfinx.io
+from dolfinx.fem import (
+    Constant,
+    Function,
+    FunctionSpace,
+    assemble_scalar,
+    dirichletbc,
+    form,
+    locate_dofs_geometrical,
+    set_bc,
+)
+import matplotlib.pyplot as plt
+import pyvista 
+from pyvista.utilities import xvfb
+
+sys.path.append('./')
+
+# meshes
+import meshes
+from meshes import primitives
+
+# visualisation
+from utils import viz
+import matplotlib.pyplot as plt
+from utils.viz import plot_mesh, plot_vector, plot_scalar
+
+# Parameters
+
+parameters = {
+    'loading': {
+        'min': 0,
+        'max': 1.5,
+        'steps': 10
+    },
+    'geometry': {
+        'geom_type': 'bar',
+        'Lx': 10.,
+        'Ly': 100.
+    },
+    'model': {
+        'E': 1.,
+        'nu': 0.,
+        'w1': 1.,
+        'ell': 0.05,
+        'k_res': 1.e-8
+    },
+    'solvers': {
+          'elasticity': {        
+            'snes': {
+                'snes_type': 'newtontr',
+                'snes_stol': 1e-8,
+                'snes_atol': 1e-8,
+                'snes_rtol': 1e-8,
+                'snes_max_it': 100,
+                'snes_monitor': "",
+                'ksp_type': 'preonly',
+                'pc_type': 'lu',
+                'pc_factor_mat_solver_type': 'mumps'
+            }
+        },
+          'damage': {        
+            'snes': {
+                'snes_type': 'vinewtonrsls',
+                'snes_stol': 1e-5,
+                'snes_atol': 1e-5,
+                'snes_rtol': 1e-8,
+                'snes_max_it': 100,
+                'snes_monitor': "",
+                'ksp_type': 'preonly',
+                'pc_type': 'lu',
+                'pc_factor_mat_solver_type': 'mumps'
+            },
+        },
+          'damage_elasticity': {
+            "max_it": 100,
+            "alpha_rtol": 1.0e-5,
+            "criterion": "alpha_H1"
+          }
+    }
+}
+# Mesh
+
+Lx = parameters["geometry"]["Lx"]
+Ly = parameters["geometry"]["Ly"]
+geom_type = parameters["geometry"]["geom_type"]
+
+
+gmsh_model, tdim = primitives.mesh_ep_gmshapi(geom_type,
+                                    Lx, 
+                                    Ly, 
+                                    1, 
+                                    0.5,
+                                    0.3, 
+                                    tdim=2)
+
+"""gmsh_model, tdim = primitives.mesh_bar_gmshapi(geom_type,
+                                    Lx, 
+                                    Ly, 
+                                    parameters.get("model").get("ell")/3, 
+                                    tdim=2)"""
+
+mesh, mts = meshes.gmsh_model_to_mesh(gmsh_model,
+                               cell_data=False,
+                               facet_data=True,
+                               gdim=2)
+
+
+from utils.viz import plot_mesh
+
+plt.figure()
+ax = plot_mesh(mesh)
+fig = ax.get_figure()
+fig.savefig(f"mesh.png")
+
+# Functional Setting
+
+element_u = ufl.VectorElement("Lagrange", mesh.ufl_cell(),
+                              degree=1, dim=2)
+
+element_alpha = ufl.FiniteElement("Lagrange", mesh.ufl_cell(),
+                              degree=1)
+
+V_u = dolfinx.fem.FunctionSpace(mesh, element_u) 
+V_alpha = dolfinx.fem.FunctionSpace(mesh, element_alpha) 
+
+u = dolfinx.fem.Function(V_u, name="Displacement")
+u_ = dolfinx.fem.Function(V_u, name="BoundaryDisplacement")
+
+
+alpha = dolfinx.fem.Function(V_alpha, name="Damage")
+
+# Pack state
+state = {"u": u, "alpha": alpha}
+
+# Bounds
+alpha_ub = dolfinx.fem.Function(V_alpha, name="UpperBoundDamage")
+alpha_lb = dolfinx.fem.Function(V_alpha, name="LowerBoundDamage")
+
+
+
+dx = ufl.Measure("dx", domain = mesh)
+ds = ufl.Measure("ds", domain = mesh)
+
+
+# Boundary sets
+
+dofs_alpha_left = locate_dofs_geometrical(V_alpha, lambda x: np.isclose(x[0], 0.))
+dofs_alpha_right = locate_dofs_geometrical(V_alpha, lambda x: np.isclose(x[0], Lx))
+dofs_alpha_bottom = locate_dofs_geometrical(V_alpha, lambda x: np.isclose(x[1], 0.))
+dofs_alpha_top = locate_dofs_geometrical(V_alpha, lambda x: np.isclose(x[1], Lx))
+
+
+dofs_u_left = locate_dofs_geometrical(V_u, lambda x: np.isclose(x[0], 0.))
+dofs_u_right = locate_dofs_geometrical(V_u, lambda x: np.isclose(x[0], Lx))
+dofs_u_top = locate_dofs_geometrical(V_u, lambda x: np.isclose(x[1], Ly))
+dofs_u_bottom = locate_dofs_geometrical(V_u, lambda x: np.isclose(x[1], 0))
+
+# Boundary data
+
+u_.interpolate(lambda x: (np.ones_like(x[0]), np.zeros_like(x[1])))
+
+# Bounds (nontrivial)
+
+alpha_lb.interpolate(lambda x: np.zeros_like(x[0]))
+alpha_ub.interpolate(lambda x: np.ones_like(x[0]))
+
+# Boundary conditions
+
+bcs_u = [
+         dirichletbc(np.array([0., 0.], dtype=PETSc.ScalarType),
+                      dofs_u_bottom,
+                      V_u),
+         dirichletbc(u_, dofs_u_top)
+         ]
+
+bcs_alpha = [
+             dirichletbc(np.array(0., dtype = PETSc.ScalarType),
+                         np.concatenate([dofs_alpha_bottom, dofs_alpha_top]),
+                         V_alpha)
+]
+
+bcs = {"bcs_u": bcs_u, "bcs_alpha": bcs_alpha}
+
+model = Brittle(parameters["model"])
+
+
+total_energy = model.total_energy_density(state) * dx
+solver = am.AlternateMinimisation(total_energy,
+                         state,
+                         bcs,
+                         parameters.get("solvers"),
+                         bounds=(alpha_lb, alpha_ub)
+                         )
+
+# Loop for evolution
+
+
+loads = np.linspace(parameters.get("loading").get("min"),
+                    parameters.get("loading").get("max"),
+                    parameters.get("loading").get("steps"))
+
+data = {
+    'elastic': [],
+    'surface': [],
+    'total': [],
+    'load': []
+}
+for (i_t, t) in enumerate(loads):
+  # update boundary conditions
+
+  u_.interpolate(lambda x: (t * np.ones_like(x[0]), np.zeros_like(x[1])))
+  u_.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                        mode=PETSc.ScatterMode.FORWARD)
+
+  # update lower bound for damage
+  alpha.vector.copy(alpha_lb.vector)
+  alpha.vector.ghostUpdate(addv=PETSc.InsertMode.INSERT,
+                        mode=PETSc.ScatterMode.FORWARD)
+
+  # solve for current load step
+  print(f"Solving timestep {i_t}, load: {t}")
+
+  solver.solve()
+
+  # postprocessing
+  # global
+
+  surface_energy = assemble_scalar(dolfinx.fem.form(model.damage_dissipation_density(state) * dx))
+
+  elastic_energy = assemble_scalar(
+        dolfinx.fem.form(model.elastic_energy_density(state) * dx))
+  
+  data.get('elastic').append(elastic_energy)
+  data.get('surface').append(surface_energy)
+  data.get('total').append(surface_energy+elastic_energy)
+  data.get('load').append(t)
+
+  print(f"Solved timestep {i_t}, load: {t}")
+  print(f"Elastic Energy {elastic_energy:.3g}, Surface energy: {surface_energy:.3g}")
+  print("\n\n")
+
+  # savings?
+plt.figure()
+plt.plot(data.get('load'), data.get('surface'), label='surface')
+plt.plot(data.get('load'), data.get('elastic'), label='elastic')
+plt.plot(data.get('load'), [1./2. * t**2*Ly for t in data.get('load')], label='anal elast', ls=':', c='k')
+
+plt.title('Traction bar energetics')
+plt.legend()
+plt.yticks([0, 1/20], [0, '$1/2.\sigma_c^2/E_0$'])
+plt.xticks([0, 1], [0, 1])
+plt.savefig("energetics.png")
+
+xvfb.start_xvfb(wait=0.05)
+pyvista.OFF_SCREEN = True
+
+plotter = pyvista.Plotter(
+        title="Displacement",
+        window_size=[1600, 600],
+        shape=(1, 2),
+    )
+
+# _plt = plot_scalar(u_.sub(0), plotter, subplot=(0, 0))
+_plt = plot_vector(u, plotter, subplot=(0, 1))
+_plt.screenshot(f"displacement_MPI.png")
+
+
+"""xvfb.start_xvfb(wait=0.05)
+pyvista.OFF_SCREEN = True
+
+plotter = pyvista.Plotter(
+        title="Displacement",
+        window_size=[1600, 600],
+        shape=(1, 2),
+    )
+
+_plt = plot_scalar(alpha.sub(0), plotter, subplot=(0, 0))
+_plt.screenshot(f"alpha.png")"""
