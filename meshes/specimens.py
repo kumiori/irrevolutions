30d204418635c2ecdb9daf3388032c390a5ecc93
andres <leon.baldelli@cnrs.fr>
2022-02-21 16:24:29 +0100

    working on kink mesh

--- meshes/specimens.py
+++ meshes/specimens.py
@@ -1,11 +1,24 @@
 from mpi4py import MPI
 
+import numpy as np
+
+
+from meshes import (
+    _addPoint,
+    _addLine,
+    _addCurveLoop,
+    _addPlaneSurface,
+    _addPhysicalGroup
+)
 
 def mesh_kink(name,
               Lx,
-              Ly,
+              theta,
+              l0,
               lc,
-              tdim,
+              tdim=2,
+              eta=1e-2,
+              Ly=1,
               order=1,
               msh_file=None,
               comm=MPI.COMM_WORLD):
@@ -28,27 +41,37 @@ def mesh_kink(name,
         model.add("Rectangle")
         model.setCurrent("Rectangle")
         # points = [p1, p2, p3, p4, p5, p6, p7, p8]
-        p1 = model.geo.addPoint(0.0, 0.0, 0, lc, tag=1)
-        p2 = model.geo.addPoint(Lx, 0.0, 0, lc, tag=2)
-        p3 = model.geo.addPoint(Lx, Ly, 0.0, lc, tag=3)
-        p4 = model.geo.addPoint(0, Ly, 0, lc, tag=4)
-        p5 = model.geo.addPoint(11.2, 18.8, 0.0, lc, tag=5)
-        p6 = model.geo.addPoint(7.5, 12.6, 0, lc, tag=6)
-        p7 = model.geo.addPoint(3.75, 6.25, 0, lc, tag=7)
-        p8 = model.geo.addPoint(7.5, 12.4, 0, lc, tag=8)
+        p1 = _addPoint(0.0, 0.0, 0, lc, tag=1)
+        p2 = _addPoint(Lx, 0.0, 0, lc, tag=2)
+        p3 = _addPoint(Lx, Ly, 0.0, lc, tag=3)
+        p4 = _addPoint(0, Ly, 0, lc, tag=4)
+
+        p5 = _addPoint(Lx/2 + l0/2 * np.cos(theta),
+                                Ly/2 + l0/2 * np.sin(theta),
+                                0.0, lc, tag=5)
+        p6 = _addPoint(Lx/2,
+                                Ly/2 + eta,
+                                0, lc, tag=6)
+        p7 = _addPoint(- (Lx/2 + l0/2 * np.cos(theta)),
+                                - (Ly/2 + l0/2 * np.sin(theta)),
+                                lc, tag=7)
+        p8 = _addPoint(Lx/2,
+                                Ly/2 - eta,
+                                0, lc, tag=8)
+
         #Les lignes = [L1, L2, L3, L4, L5, L6, L7, L8]
-        L1 = model.geo.addLine(p1, p2, tag=1)
-        L2 = model.geo.addLine(p2, p3, tag=2)
-        L3 = model.geo.addLine(p3, p4, tag=3)
-        L4 = model.geo.addLine(p4, p1, tag=4)
-        L5 = model.geo.addLine(p5, p6, tag=5)
-        L6 = model.geo.addLine(p6, p7, tag=6)
-        L7 = model.geo.addLine(p7, p8, tag=7)
-        L8 = model.geo.addLine(p8, p5, tag=8)
-        cloop1 = model.geo.addCurveLoop([L1, L2, L3, L4])
-        cloop2 = model.geo.addCurveLoop([L5, L6, L7, L8])
+        L1 = _addLine(p1, p2, tag=1)
+        L2 = _addLine(p2, p3, tag=2)
+        L3 = _addLine(p3, p4, tag=3)
+        L4 = _addLine(p4, p1, tag=4)
+        L5 = _addLine(p5, p6, tag=5)
+        L6 = _addLine(p6, p7, tag=6)
+        L7 = _addLine(p7, p8, tag=7)
+        L8 = _addLine(p8, p5, tag=8)
+        cloop1 = _addCurveLoop([L1, L2, L3, L4])
+        cloop2 = _addCurveLoop([L5, L6, L7, L8])
         # surface_1 =
-        model.geo.addPlaneSurface([cloop1, cloop2])
+        _addPlaneSurface([cloop1, cloop2])
 
         model.geo.synchronize()
         surface_entities = [model[1] for model in model.getEntities(tdim)]
@@ -77,17 +100,17 @@ def mesh_kink(name,
         # domain = 1
         # gmsh.model.addPhysicalGroup(tdim, [v[1] for v in volumes], domain)
         # gmsh.model.setPhysicalName(tdim, domain, 'domain')
-        gmsh.model.addPhysicalGroup(tdim - 1, [1], tag=9)
+        _addPhysicalGroup(tdim - 1, [1], tag=9)
         gmsh.model.setPhysicalName(tdim - 1, 9, "Bottom  of the Plate")
-        gmsh.model.addPhysicalGroup(tdim - 1, [4], tag=10)
+        _addPhysicalGroup(tdim - 1, [4], tag=10)
         gmsh.model.setPhysicalName(tdim - 1, 10, "Top  of the Plate")
-        gmsh.model.addPhysicalGroup(tdim - 1, [5], tag=11)
+        _addPhysicalGroup(tdim - 1, [5], tag=11)
         gmsh.model.setPhysicalName(tdim - 1, 11, "1/2 Top of the TipCrack")
-        gmsh.model.addPhysicalGroup(tdim - 1, [6], tag=12)
+        _addPhysicalGroup(tdim - 1, [6], tag=12)
         gmsh.model.setPhysicalName(tdim - 1, 12, "1/2 Top of the TipCrack")
-        gmsh.model.addPhysicalGroup(tdim - 1, [7], tag=13)
+        _addPhysicalGroup(tdim - 1, [7], tag=13)
         gmsh.model.setPhysicalName(tdim - 1, 13, "1/2 bottom of the TipCrack")
-        gmsh.model.addPhysicalGroup(tdim - 1, [8], tag=14)
+        _addPhysicalGroup(tdim - 1, [8], tag=14)
         gmsh.model.setPhysicalName(tdim - 1, 14, "1/2 bottom of the TipCrack")
 
         model.mesh.generate(tdim)
@@ -99,3 +122,31 @@ def mesh_kink(name,
     return gmsh.model
 
 # mesh=mesh_kink('mesh', 15, 25, 1, 2, 1,)
+if __name__ == '__main__':
+    parameters = {
+        'loading': {
+            'min': 0,
+            'max': 1.5,
+            'steps': 10
+        },
+        'geometry': {
+            'geom_type': 'slit',
+            'Lx': 1.,
+            'l0': .3,
+            'Ly': 1.,
+            'theta': 30 / 180 * 3.14,
+            'eta': 1e-2,
+        },
+        }
+
+    gmsh_model = mesh_kink('slit',
+              Lx = parameters.get('geometry').get('Lx'),
+              theta= parameters.get('geometry').get('theta'),
+              l0= parameters.get('geometry').get('l0'),
+              lc= 0.1,
+              tdim=2,
+              eta=1e-2,
+              Ly=1,
+              order=1,
+              msh_file=None,
+              comm=MPI.COMM_WORLD)
