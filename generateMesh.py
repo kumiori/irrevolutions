a32a5aa6e9c719a645cb004a27e3a4e24c429388
ueuzj <ueuzj@student.kit.edu>
2022-02-22 18:31:04 +0100

    Analysis with Castem

--- generateMesh.py
+++ generateMesh.py
@@ -9,40 +9,45 @@ from utils.viz import plot_mesh#, plot_vector, plot_scalar
 # Mesh
 Lx = 100
 Ly = 200
-s=10
-L0=20
-seedDist=1
+svals=np.arange(4, 35, step=3)
+lvals=np.arange(5, 70, step=3)
+for s in svals:
+    for L0 in lvals:
+        seedDist=1
 
-geom_type = "bar"
-gmsh_model, tdim = primitives.mesh_ep_gmshapi(geom_type,
-                                    Lx, 
-                                    Ly,
-                                    L0, 
-                                    s,   
-                                    seedDist, 
-                                    sep=0.1,
-                                    tdim=2)
-"""
-# Only needed when we are generating a mesh to validate the calculations
-gmsh_model, tdim = primitives.mesh_rightCrack_gmshapi(geom_type,
-                                    Lx, 
-                                    Ly,
-                                    L0, 
-                                    s,   
-                                    seedDist, 
-                                    sep=1,
-                                    tdim=2)
-"""
+        geom_type = "bar"
+        gmsh_model, tdim = primitives.mesh_ep_gmshapi(geom_type,
+                                            Lx, 
+                                            Ly,
+                                            L0, 
+                                            s,   
+                                            seedDist, 
+                                            sep=0.1,
+                                            tdim=2)
+        """
+        # Only needed when we are generating a mesh to validate the calculations
+        gmsh_model, tdim = primitives.mesh_rightCrack_gmshapi(geom_type,
+                                            Lx, 
+                                            Ly,
+                                            L0, 
+                                            s,   
+                                            seedDist, 
+                                            sep=1,
+                                            tdim=2)
+        """
 
-mesh, mts = meshes.gmsh_model_to_mesh(gmsh_model,
-                               cell_data=False,
-                               facet_data=True,
-                               gdim=2, 
-                               exportMesh=True, 
-                               fileName="./meshes/"+str(s)+"_"+str(L0)+".unv")
+        mesh, mts = meshes.gmsh_model_to_mesh(gmsh_model,
+                                    cell_data=False,
+                                    facet_data=True,
+                                    gdim=2, 
+                                    exportMesh=True, 
+                                    fileName="./meshes/"+str(s)+"_"+str(L0)+".unv")
 
-plt.figure()
-ax = plot_mesh(mesh)
-fig = ax.get_figure()
-fig.savefig(f"mesh.png")
+        """plt.figure()
+        ax = plot_mesh(mesh)
+        fig = ax.get_figure()
+        fig.savefig(f"mesh.png")"""
 
+for s in svals:
+    for L0 in lvals:
+        print("mesh="+str(s)+"_"+str(L0)+";")
---
+++ plotAngles.py
@@ -0,0 +1,30 @@
+"""
+Skript to plot the angles calculated with Cast3m
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    'font.family': 'serif',
+    'text.usetex': True,
+    'pgf.rcfonts': False,
+})
+
+table=np.genfromtxt("resultantAngles.dat", skip_header=1)
+svals=(np.unique(table[:, 0]))
+for s in svals:
+    plt.figure()
+    subtable=table[table[:, 0]==s]
+    plt.plot(subtable[:, 1], subtable[:, 4])
+    plt.grid()
+    plt.title("Resultant angle with different crack length, s="+str(subtable[0, 0])+"mm")
+    plt.ylim([-20, 50])
+    plt.xlabel("Crack Length, [mm]")
+    plt.ylabel("Resultant Angle, [Degrees]")
+    plt.savefig("./angleFigs/anglefigs"+str(subtable[0, 0])+".png")
+    plt.savefig("./angleFigs/anglefigs"+str(subtable[0, 0])+".pgf")
+    #plt.show()
