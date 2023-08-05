866461b2b020d3b5feeab31f7baacb8b07f6c5ae
andres <leon.baldelli@cnrs.fr>
2023-08-05 18:43:45 +0200

    first sweep of ls

--- algorithms/ls.py
+++ algorithms/ls.py
@@ -66,3 +66,58 @@ class LineSearch(object):
         # compute minimum of polynomial
 
         # return arg-minimum of 1d energy-perturbations
+
+    
+    def admissible_interval(self, state, perturbation, alpha_lb, bifurcation):
+        alpha = state["alpha"]
+        # beta = perturbation["beta"]
+        beta = bifurcation[1]
+
+        one = max(1., max(alpha.vector[:]))
+        upperbound = one
+        lowerbound = alpha_lb
+
+
+        # positive
+        mask = np.int32(np.where(beta.vector[:]>0)[0])
+
+        hp2 = (one-alpha.vector[mask])/beta.vector[mask]  if len(mask)>0 else [np.inf]
+        hp1 = (alpha_lb.vector[mask]-alpha.vector[mask])/beta.vector[mask]  if len(mask)>0 else [-np.inf]
+        hp = (max(hp1), min(hp2))
+
+        # negative
+        mask = np.int32(np.where(beta.vector[:]<0)[0])
+
+        hn2 = (one-alpha.vector[mask])/beta.vector[mask] if len(mask)>0 else [-np.inf]
+        hn1 = (alpha_lb.vector[mask]-alpha.vector[mask])/beta.vector[mask]  if len(mask)>0 else [np.inf]
+        hn = (max(hn2), min(hn1))
+
+        hmax = np.array(np.min([hp[1], hn[1]]))
+        hmin = np.array(np.max([hp[0], hn[0]]))
+
+        hmax_glob = np.array(0.0,'d')
+        hmin_glob = np.array(0.0,'d')
+
+        comm.Allreduce(hmax, hmax_glob, op=mpi4py.MPI.MIN)
+        comm.Allreduce(hmin, hmin_glob, op=mpi4py.MPI.MAX)
+
+        hmax = float(hmax_glob)
+        hmin = float(hmin_glob)
+
+
+        if hmin>0:
+            log(LogLevel.INFO, 'Line search troubles: found hmin>0')
+            return (0., 0.)
+        if hmax==0 and hmin==0:
+            log(LogLevel.INFO, 'Line search failed: found zero step size')
+            # import pdb; pdb.set_trace()
+            return (0., 0.)
+        if hmax < hmin:
+            log(LogLevel.INFO, 'Line search failed: optimal h* not admissible')
+            return (0., 0.)
+            # get next perturbation mode
+
+        assert hmax > hmin, 'hmax > hmin'
+        __import__('pdb').set_trace()
+        
+        return (hmin, hmax)
