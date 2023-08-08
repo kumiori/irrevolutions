7aa29a2621cc9333a681bc7a29ef584c12088756
andres <leon.baldelli@cnrs.fr>
2023-08-08 11:26:55 +0200

    minor on pacman

--- practice/pacman-cone.py
+++ practice/pacman-cone.py
@@ -30,6 +30,8 @@ import dolfinx
 import dolfinx.plot
 from dolfinx import log
 import ufl
+from dolfinx.mesh import locate_entities_boundary, CellType, create_rectangle
+from dolfinx.fem import locate_dofs_topological
 
 from dolfinx.fem.petsc import (
     set_bc,
@@ -48,6 +50,7 @@ from utils.plots import plot_energies
 from utils import norm_H1, norm_L2
 from meshes.pacman import mesh_pacman
 from utils.viz import plot_mesh, plot_vector, plot_scalar
+from utils.lib import _local_notch_asymptotic
 
 
 class ConvergenceError(Exception):
@@ -98,12 +101,9 @@ prefix = os.path.join(outdir, "pacman-cone")
 if comm.rank == 0:
     Path(prefix).mkdir(parents=True, exist_ok=True)
 
-def pacman_hybrid(nest):
-    # Parameters
+def pacman_cone(nest):
     Lx = 1.0
     Ly = 0.1
-    # tdim = 2
-    # _ell = 0.3
     _nel = 30
 
     with open(f"{prefix}/parameters.yaml") as f:
@@ -164,6 +164,8 @@ def pacman_hybrid(nest):
     alpha_lb = Function(V_alpha, name="Lower bound")
     alpha_ub = Function(V_alpha, name="Upper bound")
 
+
+    # Pack state
     state = {"u": u, "alpha": alpha}
 
 
@@ -212,6 +214,73 @@ def pacman_hybrid(nest):
     bcs = {"bcs_u": bcs_u, "bcs_alpha": bcs_alpha}
     bcs_z = bcs_u + bcs_alpha
 
-    
+    # Mechanical model
+
+    model = Brittle(parameters["model"])
+
+    # Energy functional
+
+    f = Constant(mesh, np.array([0, 0], dtype=PETSc.ScalarType))
+    external_work = ufl.dot(f, state["u"]) * dx
+    total_energy = model.total_energy_density(state) * dx - external_work
+
+    load_par = parameters["loading"]
+    loads = np.linspace(load_par["min"],
+                        load_par["max"], load_par["steps"])
+
+    # Solvers
+
+    solver = AlternateMinimisation(
+        total_energy, state, bcs, parameters.get("solvers"), 
+        bounds=(alpha_lb, alpha_ub)
+    )
+
+    hybrid = HybridFractureSolver(
+        total_energy,
+        state,
+        bcs,
+        bounds=(alpha_lb, alpha_ub),
+        solver_parameters=parameters.get("solvers"),
+    )
+
+    stability = StabilitySolver(
+        total_energy, state, bcs, stability_parameters=parameters.get("stability")
+    )
+
+    cone = ConeSolver(
+        total_energy, state, bcs,
+        cone_parameters=parameters.get("stability")
+    )
+
+    history_data = {
+        "load": [],
+        "elastic_energy": [],
+        "fracture_energy": [],
+        "total_energy": [],
+        "solver_data": [],
+        "cone_data": [],
+        "cone-eig": [],
+        "eigs": [],
+        "uniqueness": [],
+        "inertia": [],
+        "stable": [],
+        "F": [],    
+        "alphadot_norm" : [],
+        "rate_12_norm" : [], 
+        "unscaled_rate_12_norm" : [],
+        "cone-stable": []
+    }
+
+
+    for i_t, t in enumerate(loads):
+
+        uD.interpolate(lambda x: _local_notch_asymptotic(
+            x,
+            ω=np.deg2rad(_omega / 2.),
+            t=t,
+            par=parameters["material"]
+        ))
+
+
 if __name__ == "__main__":
-    pacman_hybrid(nest=False)
+    pacman_cone(nest=False)
