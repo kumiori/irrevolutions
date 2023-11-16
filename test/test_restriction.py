df3cdaac4204d1bb1a409024fe1ce248d32596ea
andres <leon.baldelli@cnrs.fr>
2023-11-16 15:19:04 +0100

    minor testing

--- test/test_restriction.py
+++ test/test_restriction.py
@@ -21,6 +21,7 @@ import dolfinx
 import ufl
 import numpy as np
 import random
+from test_sample_data import init_data  
 
 from petsc4py import PETSc
 from mpi4py import MPI
@@ -32,18 +33,15 @@ __log_incipit = f"rank {rank}#{size}/"
 
 from dolfinx.cpp.la.petsc import get_local_vectors, scatter_local_vectors
 
-
 def get_inactive_dofset(v, V_u = V_u, V_alpha = V_alpha):
     """docstring for get_inactive_dofset"""
     _logger.info(f"inactive dofset")
-    maps = [(V.dofmap.index_map, V.dofmap.index_map_bs) for V in [V_u, V_alpha]]
-    u, alpha = get_local_vectors(v, maps)
-    # for visibility
-    u *= 10
-    scatter_local_vectors(v, [u, alpha], maps)
-    v.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.FORWARD)
+
     V_u_size = V_u.dofmap.index_map_bs * (V_u.dofmap.index_map.size_local)
     V_alpha_size = V_alpha.dofmap.index_map_bs * (V_alpha.dofmap.index_map.size_local)
+    maps = [(V.dofmap.index_map, V.dofmap.index_map_bs) for V in [V_u, V_alpha]]
+    u, alpha = get_local_vectors(v, maps)
+    
     # simil to: admissibility
     idx_alpha_local = np.array(np.where(alpha<=1)[0], dtype=np.int32)
     idx_u_local = np.arange(V_u_size, dtype=np.int32)
@@ -51,6 +49,7 @@ def get_inactive_dofset(v, V_u = V_u, V_alpha = V_alpha):
 
     # Access the local data
     local_data = v.array
+    
     restricted_dofs = [dofs_u_all, idx_alpha_local]
 
     # Print information about the vector
@@ -70,32 +69,17 @@ def get_inactive_dofset(v, V_u = V_u, V_alpha = V_alpha):
     return restricted_dofs
     
 def test_restriction():
-    V_u_size = V_u.dofmap.index_map_bs * (V_u.dofmap.index_map.size_local)
-    V_alpha_size = V_alpha.dofmap.index_map_bs * (V_alpha.dofmap.index_map.size_local)
+    F, v = init_data(3)
 
-    F_ = [
-        ufl.derivative(
-            energy, u, ufl.TestFunction(u.ufl_function_space())
-        ),
-        ufl.derivative(
-            energy,
-            alpha,
-            ufl.TestFunction(alpha.ufl_function_space()),
-        ),
-    ]
-    F = dolfinx.fem.form(F_)
-
-    v = dolfinx.fem.petsc.create_vector_block(F)
     x = dolfinx.fem.petsc.create_vector_block(F)
-    v.array = [np.around(random.uniform(0.1, 1.5), decimals=1) for r in range(v.local_size)]
     
     restricted_dofs = get_inactive_dofset(v)
 
     constraints = restriction.Restriction([V_u, V_alpha], restricted_dofs)
     vr = constraints.restrict_vector(v)
 
-    _logger.critical(f"{__log_incipit} constraints.blocal_dofs {constraints.blocal_dofs}")
     comm.Barrier()
+    _logger.critical(f"{__log_incipit} constraints.blocal_dofs {constraints.blocal_dofs}")
     _logger.critical(f"{__log_incipit} constraints.bglobal_dofs_vec {constraints.bglobal_dofs_vec}")
     _logger.critical(f"{__log_incipit} constraints.bglobal_dofs_vec_stacked {constraints.bglobal_dofs_vec_stacked}")
     
