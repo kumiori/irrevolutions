eff590163068979e1be5e1a3fd50e4efd1035b8c
andres <leon.baldelli@cnrs.fr>
2023-11-17 19:04:01 +0100

    extension seems to work Q&D, now ask advice

--- test/test_extend.py
+++ test/test_extend.py
@@ -1,17 +1,3 @@
-from test_scatter_MPI import (
-    mesh,
-    element_alpha,
-    element_u,
-    V_alpha,
-    V_u,
-    alpha,
-    u,
-    dofs_alpha_left,
-    dofs_alpha_right,
-    dofs_u_right,
-    dofs_u_left,
-    energy)
-
 import os
 import sys
 sys.path.append("../")
@@ -31,49 +17,115 @@ size = comm.Get_size()
 
 from test_restriction import (
     __log_incipit,
-    test_restriction,
     get_inactive_dofset,
-    
 )
 
-def test_extension(v, vr, constraints):
-    F_ = [
-        ufl.derivative(
-            energy, u, ufl.TestFunction(u.ufl_function_space())
-        ),
-        ufl.derivative(
-            energy,
-            alpha,
-            ufl.TestFunction(alpha.ufl_function_space()),
-        ),
-    ]
-    F = dolfinx.fem.form(F_)
+from dolfinx.cpp.la.petsc import get_local_vectors, scatter_local_vectors
+from test_sample_data import init_data  
 
-    x = dolfinx.fem.petsc.create_vector_block(F)
+def test_extension(vr, x, constraints):
+    V_u, V_alpha = constraints.function_spaces[0], constraints.function_spaces[1]
+    maps = [(V.dofmap.index_map, V.dofmap.index_map_bs) for V in [V_u, V_alpha]]
+
     _logger.info(f"x")
     x.view()
+
+    # _logger.info(f"v")
+    # v.view()
+    
+    _logger.info(f"vr")
+    vr.view()
     
     _logger.info(f"setting up dofs for extension")
     _logger.critical(f"{__log_incipit} The \"good\" dofs: constraints.bglobal_dofs_vec_stacked {constraints.bglobal_dofs_vec_stacked}")
+
+    x.zeroEntries()
+    x.array[constraints.bglobal_dofs_vec_stacked] = vr.array
+    _logger.critical(f"{__log_incipit} Local data of the x: {x.array}")
+    # _logger.info(f"v")
+    # v.view()
+    _logger.info(f"x")
+    x.view()
+    x_u, x_alpha = get_local_vectors(x, maps)
+    _logger.info(f"The local vectors")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_u: {x_u}")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_alpha: {x_alpha}")
+
+def sandbox():
     
-    _islocal = PETSc.IS().createGeneral(constraints.bglobal_dofs_vec_stacked)
-    _logger.info(f"_islocal")
-    _islocal.view()
-    # _islocal is the inverse mapping from the global dofs of the entire vector to the local dofs of the restricted vector 
-    # I need, not only the target degrees of freedom, but also the index set mapping
+    extension = PETSc.Scatter().create(vr, is_from = None, vec_to = x, is_to = _islocal)
+    extension.view()
+    # _islocal = PETSc.IS().createGeneral(constraints.bglobal_dofs_vec_stacked)
+    # _logger.info(f"_islocal")
+    # _islocal.view()
+
+
+    # extension.scatter(vr, x, False, PETSc.ScatterMode.REVERSE)
+    # _logger.info(f"x: we have scattered the \"good\" values into x REVERSE")
+    # x.view()
+
+    # x_u, x_alpha = get_local_vectors(x, maps)
+    # comm.Barrier()
+    # _logger.info(f"The local vectors")
+    # _logger.critical(f"{__log_incipit} Local data of the subvector x_u: {x_u}")
+    # _logger.critical(f"{__log_incipit} Local data of the subvector x_alpha: {x_alpha}")
     
-    _subvector = v.getSubVector(_islocal)
-    _logger.info(f"_subvector contains the values on the \"good\" dofs")
-    _logger.info(f"_subvector")
-    _subvector.view()
-    # now I want to scatter the values from _subvector into x
-    # scatter(_islocal, _subvector, x)
-    scatter = PETSc.Scatter().create(vr, is_from = None, vec_to = x, is_to = _islocal)
-    scatter.view()
-    scatter.scatter(vr, x, False, PETSc.ScatterMode.FORWARD)
-    _logger.info(f"we have scattered the \"good\" values into x")
+    # extension.scatter(vr, x, False, PETSc.ScatterMode.FORWARD)
+    extension.scatter(vr, x, addv = PETSc.InsertMode.INSERT, mode = PETSc.ScatterMode.FORWARD)
+    _logger.info(f"x: we have scattered the \"good\" values into x with INSERT & FORWARD")
+    x.view()
+    x_u, x_alpha = get_local_vectors(x, maps)
+    comm.Barrier()
+    _logger.info(f"The local vectors")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_u: {x_u}")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_alpha: {x_alpha}")
+    comm.Barrier()
+
+    
+    x.zeroEntries()
+    extension.scatter(vr, x, addv = PETSc.InsertMode.INSERT, mode = PETSc.ScatterMode.REVERSE)
+    _logger.info(f"x: we have scattered the \"good\" values into x with INSERT & REVERSE")
     x.view()
     
+    x_u, x_alpha = get_local_vectors(x, maps)
+
+    _logger.info(f"The local vectors")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_u: {x_u}")
+    _logger.critical(f"{__log_incipit} Local data of the subvector x_alpha: {x_alpha}")
+    comm.Barrier()
+
+    # x.zeroEntries()
+    # extension.scatter(vr, x, False, PETSc.ScatterMode.SCATTER_FORWARD_LOCAL)
+    # _logger.info(f"x: we have scattered the \"good\" values into x SCATTER_FORWARD_LOCAL")
+    # x.view()
+ 
+    # x_u, x_alpha = get_local_vectors(x, maps)
+    # comm.Barrier()
+    # _logger.info(f"The local vectors")
+    # _logger.critical(f"{__log_incipit} Local data of the subvector x_u: {x_u}")
+    # _logger.critical(f"{__log_incipit} Local data of the subvector x_alpha: {x_alpha}")
+ 
+    
+    _logger.info(f"The original vector")
+    v.view()
+    v_u, v_alpha = get_local_vectors(v, maps)
+    comm.Barrier()
+    _logger.info(f"The local vectors")
+    _logger.critical(f"{__log_incipit} Local data of the subvector v_u: {v_u}")
+    _logger.critical(f"{__log_incipit} Local data of the subvector v_alpha: {v_alpha}")
+   
 if __name__ == "__main__":
-    v, vr, constraints = test_restriction()
-    test_extension(v, vr, constraints)
+    F, v = init_data(10)
+    V_u, V_alpha = F[0].function_spaces[0], F[1].function_spaces[0]
+    x = dolfinx.fem.petsc.create_vector_block(F)
+    
+    restricted_dofs = get_inactive_dofset(v, F)
+    
+    constraints = restriction.Restriction([V_u, V_alpha], restricted_dofs)
+
+    _logger.critical(f"{__log_incipit} constraints.blocal_dofs {constraints.blocal_dofs}")
+    _logger.critical(f"{__log_incipit} constraints.bglobal_dofs_vec {constraints.bglobal_dofs_vec}")
+    _logger.critical(f"{__log_incipit} constraints.bglobal_dofs_vec_stacked {constraints.bglobal_dofs_vec_stacked}")
+    
+    vr = constraints.restrict_vector(v)
+    test_extension(vr, x, constraints)
