d47702ee6bd9830b6c03754643351110c95e5834
Andrés A León Baldelli <leon.baldelli@cnrs.fr>
2024-03-13 16:49:59 +0100

    successfully packaged

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='irrevolution',
     version='2024.0.1',
+    package_dir={'': 'src'},  # The root package is under the 'src' directory
     packages=find_packages('src'),  # Find packages under the 'src' directory
-    package_dir={'': 'src'},  # Specify that the root package is under the 'src' directory
 )
