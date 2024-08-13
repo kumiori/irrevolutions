3775b17bc3587b1b71d8aaa0c594d260a7a10333
kumiori <a.leon.baldelli@gmail.com>
2024-08-13 09:54:31 +0300

    ruff format

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='irrevolutions',
-    version='2024.0.1',
-    package_dir={'': 'src'},  # The root package is under the 'src' directory
-    packages=find_packages('src'),  # Find packages under the 'src' directory
+    name="irrevolutions",
+    version="2024.0.1",
+    package_dir={"": "src"},  # The root package is under the 'src' directory
+    packages=find_packages("src"),  # Find packages under the 'src' directory
     include_package_data=True,
     package_data={
-        'irrevolutions.models': ['default_parameters.yml'],
+        "irrevolutions.models": ["default_parameters.yml"],
     },
 )
